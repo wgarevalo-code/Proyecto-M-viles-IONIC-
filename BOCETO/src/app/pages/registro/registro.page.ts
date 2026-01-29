cd7b08af67f1a7e056f27ce7c28eb9462e64d1f8
Vdcarvajal-spec <vdcarvajal@sudamericano.edu.ec>
2026-01-28 20:20:06 -0500

    Agregado registro con Google

--- BOCETO/src/app/pages/registro/registro.page.ts
+++ BOCETO/src/app/pages/registro/registro.page.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular'; // <--- 1. Importamos esto
-import { RouterModule } from '@angular/router'; // <--- 2. Para que funcionen los enlaces
+import { IonicModule } from '@ionic/angular'; 
+import { RouterModule } from '@angular/router';
+// 1. Importamos las herramientas para los iconos
+import { addIcons } from 'ionicons';
+import { logoGoogle } from 'ionicons/icons';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
   styleUrls: ['./registro.page.scss'],
   standalone: true,
-  // 3. AGREGAMOS IonicModule y RouterModule AQUÍ ABAJO:
   imports: [IonicModule, CommonModule, FormsModule, RouterModule] 
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    // 2. Registramos el icono de Google para que Ionic lo reconozca
+    addIcons({ logoGoogle });
+  }
 
   ngOnInit() {
+  }
+
+  // 3. Agregamos la función que se ejecutará al hacer clic
+  async registroGoogle() {
+    console.log('Botón de Google presionado');
+    // Nota: Aquí irá la lógica de Firebase más adelante
+    alert('Conexión con Google iniciada (falta configurar Firebase)');
   }
 
 }
